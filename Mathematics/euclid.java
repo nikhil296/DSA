746e564ae18c3ebca231f8db981c835d7d22643e
nikhil296 <nikhilgautam1729@gmail.com>
2022-03-22 15:11:43 +0530

    Euclid GCD added

--- Mathematics/euclid.java
+++ Mathematics/euclid.java
@@ -1,3 +1,22 @@
-public class euclid {
-    
+class euclid {
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b%a, a);
+    }
+    public static void main(String[] args)
+    {
+        int a = 10, b = 15, g;
+        g = gcd(a, b);
+        System.out.println("GCD(" + a +  " , " + b+ ") = " + g);
+         
+        a = 35; b = 10;
+        g = gcd(a, b);
+        System.out.println("GCD(" + a +  " , " + b+ ") = " + g);
+         
+        a = 31; b = 2;
+        g = gcd(a, b);
+        System.out.println("GCD(" + a +  " , " + b+ ") = " + g);
+    }
 }
